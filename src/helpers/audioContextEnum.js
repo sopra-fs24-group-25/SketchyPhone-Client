f8efebaba5085d0623e36b4b0a58118bb4d7bfe1
noah.isaak <noah.isaak@uzh.ch>
2024-05-20 17:30:40 +0200

    Fix test for audio sources not uploaded properly

--- src/helpers/audioContextEnum.js
+++ src/helpers/audioContextEnum.js
@@ -1,10 +1,16 @@
+import scribble from "../audio/scribble.mp3";
+import upvote from "../audio/upvote.mp3";
+import analogRing from "../audio/analogRing.mp3";
+import clickPop from "../audio/clickPop.mp3";
+import downvote from "../audio/downvote.mp3";
+
 const AudioContextEnum = {
-    PRESENTATION_REVEAL_DRAWING: { repr: "PRESENTATION_REVEAL_DRAWING", url: "/audio/scribble.mp3" },
-    UPVOTE: { repr: "UPVOTE", url: "/audio/upvote.mp3" },
-    DOWNVOTE: { repr: "DOWNVOTE", url: "/audio/downvote.mp3" },
+    PRESENTATION_REVEAL_DRAWING: { repr: "PRESENTATION_REVEAL_DRAWING", url: scribble },
+    UPVOTE: { repr: "UPVOTE", url: upvote },
+    DOWNVOTE: { repr: "DOWNVOTE", url: downvote },
     BUTTON_STANDARD: { repr: "BUTTON_STANDARD", url: "" },
-    BUTTON_POP: {repr: "BUTTON_POP", url: "/audio/clickPop.mp3"},
-    TIMER: {repr: "TIMER", url:"/audio/analogRing.mp3"}
+    BUTTON_POP: { repr: "BUTTON_POP", url: clickPop },
+    TIMER: { repr: "TIMER", url: analogRing }
 }
 
 export default AudioContextEnum;
